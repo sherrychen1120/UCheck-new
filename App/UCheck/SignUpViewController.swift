03321c01e478307b460aa61ad2ae7119858f2ec6
sherrychen1120 <sherrychen961120@gmail.com>
2017-11-12 03:28:49 -0500

    Signup VC first version

---
+++ App/UCheck/SignUpViewController.swift
@@ -0,0 +1,133 @@
+//
+//  SignUpViewController.swift
+//  UCheck
+//
+//  Created by Sherry Chen on 11/12/17.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import SwiftKeychainWrapper
+
+class SignUpViewController: UIViewController {
+
+    @IBAction func FBLoginButton(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var FirstNameInput: UITextField!
+    @IBOutlet weak var LastNameInput: UITextField!
+    @IBOutlet weak var EmailInput: UITextField!
+    @IBOutlet weak var PasswordInput: UITextField!
+    
+    let ref = FIRDatabase.database().reference(withPath: "user-profiles")
+    var new_user = User(uid : "", first_name: "", last_name: "", email: "", phone_no: "")
+    var first_name = ""
+    var last_name = ""
+    var email = ""
+    var password = ""
+    var signUpSuccess = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SignUpButton(_ sender: Any) {
+        first_name = FirstNameInput.text!
+        last_name = LastNameInput.text!
+        email = EmailInput.text!
+        password = PasswordInput.text!
+        
+        if (first_name == "" || last_name == "" || email == "" || password == "" ){
+            self.showAlert(withMessage: "Incomplete Information")
+        } else {
+            //Firebase Auth - create new user
+            FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    
+                    if let errCode = FIRAuthErrorCode(rawValue: error._code) {
+                        
+                        switch errCode {
+                        case .errorCodeInvalidEmail:
+                            self.showAlert(withMessage: "Invalid email.")
+                        case .errorCodeEmailAlreadyInUse:
+                            self.showAlert(withMessage: "Email already in use.")
+                        case .errorCodeWeakPassword:
+                            self.showAlert(withMessage: "Password should contain at least 6 characters.")
+                        default:
+                            print("Create User Error: \(error)")
+                        }
+                    }
+                    
+                } else {
+                    FIRAuth.auth()!.signIn(withEmail: self.email, password: self.password){(user, error) in
+                        
+                        if (error != nil){
+                            print(error?.localizedDescription)
+                        } else {
+                            //fill the info to the new_user object
+                            var uid = ""
+                            if let user = FIRAuth.auth()?.currentUser{
+                                uid = user.uid
+                                CurrentUserId = user.uid
+                            }
+                            self.new_user = User(uid : uid,
+                                                 first_name : self.first_name,
+                                                 last_name : self.last_name,
+                                                 email : self.email,
+                                                 phone_no : "")
+                            
+                            //update info on Firebase
+                            let user_ref = self.ref.child(uid)
+                            user_ref.setValue(self.new_user.toAnyObject())
+                            
+                            //update info in the CurrentSession object
+                            CurrentUser = self.email
+                            CurrentUserName = self.first_name + " " + self.last_name
+                            
+                            let saveEmail: Bool = KeychainWrapper.standard.set(self.email, forKey: "email")
+                            let savePassword: Bool = KeychainWrapper.standard.set(self.password, forKey: "password")
+                            print("Successfully saved email: \(saveEmail);")
+                            print("Successfully saved passwordd: \(savePassword).")
+                            
+                            self.performSegue(withIdentifier: "SignUpToVenmo", sender: nil)
+                            
+                        }
+                        
+                    }
+                    
+                }//If
+            }//FIRAutho
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+        
+        // MARK: - show alert
+        func showAlert(withMessage: String) {
+            let alert = UIAlertController(title: "Eh Oh", message: withMessage, preferredStyle: UIAlertControllerStyle.alert)
+            let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
