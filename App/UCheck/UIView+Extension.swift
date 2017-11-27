9fc86c71e3ea902dd6788a0bcbc89a71703285f3
Ezaan Mangalji <emangalji@gmail.com>
2017-11-27 02:28:50 -0500

    UI of login screen remodel

--- App/UCheck/UIView+Extension.swift
+++ App/UCheck/UIView+Extension.swift
@@ -14,6 +14,13 @@ extension UIView {
         
         layer.insertSublayer(gradientLayer, at: 0)
     }
+    
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
 }
 
 struct Colors {
